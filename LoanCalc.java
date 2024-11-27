f3d1160438ff7ec730f4bf92cdd38a2f427cabdf
Shira Boodaei <shiraboodaei@Shiras-MacBook-Air.local>
2024-11-27 16:58:09 +0200

    Create loanCalc

--- LoanCalc.java
+++ LoanCalc.java
@@ -27,9 +27,13 @@ public class LoanCalc {
 
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
-	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+	private static double endBalance(double loan, double rate, int n, double payment) {
+		double leftPrice = loan;
+		for (int i = 0; i < n; i ++){
+			leftPrice = (leftPrice - payment) * (1 + (rate)/100);
+
+		}
+		return leftPrice;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,8 +42,14 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double g = loan/ (double) n;
+		iterationCounter = 0;
+		while (endBalance(loan, rate, n, g) > 0) {
+			g = g + epsilon;
+			iterationCounter++;
+			}
+
+		return g;
     }
     
     // Uses bisection search to compute an approximation of the periodical payment 
@@ -48,7 +58,17 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+		double lo = loan/ (double) n;
+		double hi = loan*2; 
+		double g = (lo+hi)/2; 
+		iterationCounter = 0;
+		while ((hi - lo) > epsilon){
+			if ((endBalance(loan, rate, n, g) * endBalance(loan, rate, n, lo)) > 0){
+				lo = g;
+			} hi = g;
+			g = (lo+hi)/2;
+			iterationCounter ++;
+		}
+		return g;
     }
 }
